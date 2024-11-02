0e2d942639a6b6fd9328173626e557e46d73045b
Tasnuva <tasnuva.contact@gmail.com>
2024-11-03 00:28:45 +0600

    stack

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,24 @@
 use anyhow::{Context, Result};
-use std::io::{self, BufRead, Read};
+use std::collections::binary_heap;
+use std::env;
+use std::io::BufRead;
 use std::io::{stdin, stdout, Cursor, Write};
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
     loop {
         print!("$ ");
         let mut s = String::new();
         let _ = stdout().flush();
         stdin()
             .read_line(&mut s)
-            .context("Did not enter a correct string");
-        // match s.as_str() {
-        //     // "exit" => ,
-        //     "" => println!("{}: command not found", s.trim()),
-        //     _ => {
-
-        //     }
-        // }
+            .context("Did not enter a correct string")
+            .ok();
 
         let cursor = Cursor::new(s.as_bytes());
-        let mut split_iter = cursor.split(b' ').map(|v| v.unwrap());
+        let split_iter = cursor.split(b' ').map(|v| v.unwrap());
 
         let mut arguments_vec = Vec::new();
-        let args = split_iter
-            .into_iter()
-            .map(|v| String::from_utf8(v).unwrap());
+        let args = split_iter.map(|v| String::from_utf8(v).unwrap());
 
         args.into_iter().for_each(|e| arguments_vec.push(e));
 
@@ -38,6 +31,78 @@ fn main() -> Result<()> {
                     };
                 } else if arguments_vec.len() == 1 {
                     std::process::exit(0);
+                }
+            }
+            "echo" => {
+                if arguments_vec.len() >= 2 {
+                    let mut stdout = std::io::stdout().lock();
+
+                    arguments_vec[1..].into_iter().for_each(|e| {
+                        stdout.write(e.as_bytes()).context("stdout write error");
+                        stdout.write(b" ").context("stdout write error");
+                    });
+                    // stdout.write_all(b"some bytes").unwrap();
+                    // println!("{:?}", arguments_vec);
+                } else if arguments_vec.len() == 1 {
+                    println!();
+                }
+            }
+            "type" => {
+                if arguments_vec.len() >= 2 {
+                    arguments_vec[1..].into_iter().for_each(|e| {
+                        let e = e.trim();
+                        if e == "exit" {
+                            println!("exit is a shell builtin")
+                        } else if e == "echo" {
+                            println!("echo is a shell builtin")
+                        } else if e == "type" {
+                            println!("type is a shell builtin")
+                        } else {
+                            // println!("{}: not found", e)
+                        }
+                    });
+
+                    let key = "PATH";
+                    let mut result_path = String::new();
+                    match env::var_os(key) {
+                        Some(paths) => {
+                            for path in env::split_paths(&paths) {
+                                if path.is_dir() {
+                                    for entry in std::fs::read_dir(path)? {
+                                        // let entry = entry?;
+                                        let path = entry.as_ref().unwrap().path();
+                                        arguments_vec[1..].iter().for_each(|file| {
+                                            if path.file_name().unwrap().to_str().unwrap()
+                                                == file.as_str().trim()
+                                            {
+                                                result_path.push_str(file.as_str().trim());
+                                                result_path.push_str(" is ");
+                                                result_path.push_str(path.to_str().unwrap());
+
+                                                // println!(
+                                                //     "{} is {}",
+                                                //     file.as_str().trim(),
+                                                //     path.to_str().unwrap()
+                                                // );
+                                            } else {
+                                                println!("{} not found", file.as_str().trim());
+                                                // uncommant
+                                            }
+                                        })
+                                    }
+                                }
+                            }
+                        }
+
+                        None => println!("{key} is not defined in the environment."),
+                    }
+                    // println!("{:?}", result_path);
+                    match result_path.as_str() {
+                        "" => println!("{} not found", arguments_vec[1].trim()),
+                        _ => println!("{}", result_path),
+                    }
+                } else if arguments_vec.len() == 1 {
+                    println!();
                 }
             }
             "" => println!("Please enter a command"),
